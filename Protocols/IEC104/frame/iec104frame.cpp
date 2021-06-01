e23bd7a65438ca677b5f51352bf98fa746a3921c
CGinMax <cginmax1998@gmail.com>
2021-06-01 17:44:46 +0800

    update

--- Protocols/IEC104/frame/iec104frame.cpp
+++ Protocols/IEC104/frame/iec104frame.cpp
@@ -26,3 +26,112 @@ uint8_t IEC104VSQ::toByte()
 
 std::string IEC104VSQ::toString()
 {}
+
+IEC104Cause::IEC104Cause(uint8_t reason, uint8_t srcAddr)
+{
+    m_T = (reason >> 7) & 0x01;
+    m_POrN = (reason >> 6) & 0x01;
+    m_reason = reason & 0x3F;
+    m_srcAddr = srcAddr;
+}
+
+std::vector<uint8_t> IEC104Cause::toByte()
+{
+    std::vector<uint8_t> result;
+    uint8_t primaryByte = 0;
+    primaryByte |= (m_T << 7);
+    primaryByte |= (m_POrN << 6);
+    primaryByte |= (m_reason & 0x3F);
+    result.push_back(primaryByte);
+    result.push_back(m_srcAddr);
+    return result;
+}
+
+std::string IEC104Cause::toString()
+{
+
+}
+
+IEC104Quality::IEC104Quality(uint8_t quality)
+{}
+
+IEC104Quality::~IEC104Quality()
+{}
+
+uint8_t IEC104Quality::toByte()
+{
+    uint8_t result = m_valid ? 0x00 : 0x80;
+    result |= (m_data & 0x7F);
+    return result;
+}
+
+std::string IEC104SingleYXQuality::toString()
+{
+
+}
+
+std::string IEC104TwiceYXQuality::toString()
+{}
+
+IEC104Time::IEC104Time()
+{}
+
+IEC104Time::~IEC104Time()
+{}
+
+std::vector<uint8_t> IEC104Time::toBytes()
+{
+    std::vector<uint8_t> result;
+    result.push_back(m_msec & 0xFF);
+    result.push_back((m_msec >> 8) & 0xFF);
+    uint8_t byte = m_valid ? 0x00 : 0x80;
+    byte |= m_minute;
+    result.push_back(byte);
+    return result;
+}
+
+std::string IEC104Time::toString()
+{}
+
+IEC104LongTime::IEC104LongTime()
+{}
+
+std::vector<uint8_t> IEC104LongTime::toBytes()
+{
+    auto result = IEC104Time::toBytes();
+    result.push_back(m_hour & 0x1F);
+    uint8_t weekday = 0;
+    weekday |= (m_week << 5);
+    weekday |= (m_day & 0x1F);
+    result.push_back(weekday);
+    result.push_back(m_mounth & 0x0F);
+    result.push_back(m_year & 0x7F);
+    return result;
+}
+
+std::string IEC104LongTime::toString()
+{}
+
+IEC104YCInfo::IEC104YCInfo()
+{}
+
+IEC104YCInfo::~IEC104YCInfo()
+{}
+
+std::vector<uint8_t> IEC104YCInfo::toBytes()
+{}
+
+std::string IEC104YCInfo::toString()
+{}
+
+IEC104YXInfo::IEC104YXInfo()
+{}
+
+IEC104YXInfo::~IEC104YXInfo()
+{}
+
+std::vector<uint8_t> IEC104YXInfo::toBytes()
+{}
+
+std::string IEC104YXInfo::toString()
+{}
--- Protocols/IEC104/frame/iec104frame.h
+++ Protocols/IEC104/frame/iec104frame.h
@@ -8,7 +8,74 @@
 #include "iec104enums.h"
 #include "iec104apci.h"
 
-class IEC104VSQ;
+class IEC104VSQ
+{
+public:
+    explicit IEC104VSQ(uint8_t vsqByte = 0);
+
+    IEC104VSQ(const IEC104VSQ& other) = default;
+    IEC104VSQ& operator=(const IEC104VSQ& other) = default;
+
+    uint8_t toByte();
+
+    std::string toString();
+
+    uint8_t m_num;
+    uint8_t m_sq;
+};
+
+struct IEC104Cause
+{
+    explicit IEC104Cause(uint8_t reason = 0, uint8_t srcAddr = 0);
+    uint8_t m_T;
+    uint8_t m_POrN;
+    uint8_t m_reason;
+    uint8_t m_srcAddr;
+
+    std::vector<uint8_t> toByte();
+    std::string toString();
+};
+
+class IEC104Quality
+{
+public:
+    explicit IEC104Quality(uint8_t quality = 0);
+    virtual ~IEC104Quality();
+
+    virtual uint8_t toByte();
+    virtual std::string toString() = 0;
+
+    bool m_valid;
+    uint8_t m_data;
+};
+
+class IEC104Time
+{
+public:
+    IEC104Time();
+    virtual ~IEC104Time();
+
+    virtual std::vector<uint8_t> toBytes();
+    virtual std::string toString();
+
+    uint16_t m_msec;
+    bool m_valid;
+    uint8_t m_minute;
+};
+
+class IEC104Info
+{
+public:
+    IEC104Info();
+    virtual ~IEC104Info();
+
+    virtual std::vector<uint8_t> toBytes() = 0;
+    virtual std::string toString() = 0;
+
+    int m_addr;
+
+    std::shared_ptr<IEC104Quality> m_quality;
+};
 
 class PROTOCOLSSHARED_EXPORT IEC104Frame
 {
@@ -25,36 +92,69 @@ public:
 
     IEC104VSQ m_vsq;
 
-    uint16_t m_reason;
+    IEC104Cause m_cause;
 
     uint16_t m_pubAddr;
 
-
-
 };
 
-class IEC104VSQ
+class IEC104SingleYXQuality : public IEC104Quality
 {
 public:
-    explicit IEC104VSQ(uint8_t vsqByte);
-
-    IEC104VSQ(const IEC104VSQ& other) = default;
-    IEC104VSQ& operator=(const IEC104VSQ& other) = default;
-
-    uint8_t toByte();
-
-    std::string toString();
-
-    uint8_t m_num;
-    uint8_t m_sq;
+    using IEC104Quality::IEC104Quality;
+    std::string toString() override;
 };
 
-struct IEC104Cause
+class IEC104TwiceYXQuality : public IEC104Quality
 {
-
-    uint8_t m_reason;
-    std::vector<uint8_t> toByte();
-    std::string toString();
+public:
+    using IEC104Quality::IEC104Quality;
+    std::string toString() override;
 };
+
+class IEC104LongTime : public IEC104Time
+{
+public:
+    IEC104LongTime();
+
+    std::vector<uint8_t> toBytes() override;
+
+    std::string toString() override;
+
+    uint8_t m_hour;
+
+    uint8_t m_week;
+
+    uint8_t m_day;
+
+    uint8_t m_mounth;
+
+    uint8_t m_year;
+};
+
+class IEC104YCInfo : public IEC104Info
+{
+public:
+    IEC104YCInfo();
+    ~IEC104YCInfo() override;
+
+    std::vector<uint8_t> toBytes() override;
+    std::string toString() override;
+
+    double m_value;
+};
+
+class IEC104YXInfo : public IEC104Info
+{
+public:
+    IEC104YXInfo();
+    ~IEC104YXInfo() override;
+
+    std::vector<uint8_t> toBytes() override;
+    std::string toString() override;
+
+    bool m_value;
+};
+
 
 #endif // IEC104FRAME_H
